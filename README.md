a7f6c15deb4f64e4a46e8f1d302da703df444619
Jinx-FX <2674802603@qq.com>
2023-01-06 00:16:41 +0800

    my react first project done

--- README.md
+++ README.md
@@ -8,14 +8,17 @@
   - React 官方脚手架 create-react-app
   - react hooks
   - 状态管理：mobx
-  - UI 组件库：antd v4
+  - UI 组件库：antd v5
   - ajax请求库：axios
   - 路由：react-router-dom 以及 history
   - 富文本编辑器：react-quill
   - CSS 预编译器：sass
   - ...
+- 优化
+  - CDN 加速
+  - 路由懒加载
 
-# Reference
+## Reference
 
 - [react-pc-code](https://gitee.com/react-cp/react-pc-code)
 - [React-极客园PC项目](https://www.yuque.com/fechaichai/tzzlh1)
--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 // 导入路由
 import { Route, Routes } from 'react-router-dom'
 // 导入页面组件
-import Login from '@/pages/Login'
-import Layout from '@/pages/Layout'
 import { AuthRoute } from '@/components'
 import './App.scss'
-import Home from '@/pages/Home'
-import Article from './pages/Article'
-import Publish from './pages/Publish'
 import { HistoryRouter, history } from '@/utils'
+// * 路由懒加载
+// 导入必要组件
+import { lazy, Suspense } from 'react'
+// 按需导入路由组件
+const Login = lazy(() => import('./pages/Login'))
+const Layout = lazy(() => import('./pages/Layout'))
+const Home = lazy(() => import('./pages/Home'))
+const Article = lazy(() => import('./pages/Article'))
+const Publish = lazy(() => import('./pages/Publish'))
 
 // 配置路由规则
 function App () {
   return (
     <HistoryRouter history={history}>
-      <div className="App">
-        <Routes>
-          {/* 需要鉴权的路由 */}
-          <Route
-            path="/"
-            element={
-              <AuthRoute>
-                <Layout />
-              </AuthRoute>
-            }
+      <Suspense
+        fallback={
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: 200
+            }}
           >
-            <Route index element={<Home />} />
-            <Route path='/article' element={<Article />} />
-            <Route path='/publish' element={<Publish />} />
-          </Route>
-          {/* 不需要鉴权的路由 */}
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </div>
+            loading...
+          </div>
+        }
+      >
+        <div className="App">
+          <Routes>
+            {/* 需要鉴权的路由 */}
+            <Route
+              path="/"
+              element={
+                <AuthRoute>
+                  <Layout />
+                </AuthRoute>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path='/article' element={<Article />} />
+              <Route path='/publish' element={<Publish />} />
+            </Route>
+            {/* 不需要鉴权的路由 */}
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </Suspense>
     </HistoryRouter>
   )
 }
